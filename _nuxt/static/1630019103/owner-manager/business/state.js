b35da2380704b3dde6411781fcf9305e3cca79e8
benson momodu <benson.momodu@minyx360.com>
2021-08-27 00:15:36 +0100

    .......

---
+++ _nuxt/static/1630019103/owner-manager/business/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1630019103",layout:"default",error:a,state:a,serverRendered:false,routePath:"\u002Fowner-manager\u002Fbusiness",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
